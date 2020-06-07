94980e6ea698eebe2d33c189d03aadb8d34a437c
hamzazaheer721 <hamzazaheer721@gmail.com>
2020-06-07 11:00:28 +0500

    Quiz App Project (Updated)

--- App.js
+++ App.js
@@ -1,90 +1,242 @@
-import 'react-native-gesture-handler';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-import React,{Component} from 'react';
-import continent from './src/Tabs';
-import Worldwide from './src/Worldwide';
-import Home from './src/countriesList';
-import { createDrawerNavigator,drawerBackgroundColor,drawerLabel,DrawerMenu } from '@react-navigation/drawer';
-import ByCountry from './src/countryDetails'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ImageBackground } from 'react-native';
+import { Questions } from './Components/Questions';
 
-const Drawer = createDrawerNavigator();
-const Stack = createStackNavigator();
+class App extends React.Component{
 
-class Extreme extends Component {
-  render(){
-    return(
-  <Stack.Navigator>
-      <Stack.Screen 
-      name="Home" 
-      component={Home} 
-      options={{ title: 'Corona Virus Statistics',
-        headerTitleAlign: 'center',
-        headerTintColor: 'black',
-        headerStyle: {backgroundColor: '#feda6a'},
-        headerLeft: () => (
-        <Icon name="menu" size={30} style={{marginLeft: 20 ,backgroundColor: '#feda6a', color : "blue"}} onPress={() => this.props.navigation.openDrawer()} color='white'></Icon>
-    ), }} />
-      
-       <Stack.Screen name="ByCountry" 
-       component={ByCountry}
-        options={{ title: 'Country',
-        headerTitleAlign: 'left',
-        headerTintColor: 'white',
-        headerStyle: {backgroundColor: '#ec7f37'},
-        headerRight: () => (
-        <Icon name="menu" size={30} style={{marginRight: 20}} onPress={() => this.props.navigation.openDrawer()} color='white'></Icon>
-    ), }} />
-      <Stack.Screen 
-      name="Worldwide" 
-      backgroundColor='black'
-      component={Worldwide} 
-      options={{ title: 'Worldwide',
-      headerTintColor: 'white',    
-    }} 
-      />
-      <Stack.Screen     
-      name="continent"
-      component={continent} 
-      options={{ title: 'continent',
-      headerTintColor: 'black',
-       headerStyle: {backgroundColor: 'black'}
-      }}
-      />
-  </Stack.Navigator> 
-    );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      screen: 'start',
+      press: false,
+      options: [],
+      count: 1,
+      selector: parseInt(Math.random()*15),
+      selected: [],
+      isCorrect: ['#1d1e22','#1d1e22','#1d1e22','#1d1e22'],
+      score: 0
     }
-}
 
+    this.checkAnswer=this.checkAnswer.bind(this)
+  }
 
+  startquiz = () =>{
+    this.setState({
+      screen: 'quiz',
+      question: Questions[this.state.selector].question,
+      options: Questions[this.state.selector].options,
+      answer: Questions[this.state.selector].answer,
+      selected: [this.state.selector],
+      selector: parseInt(Math.random()*15),
+    })
+  }
 
-export default class App extends Component{
+  proceed = () =>{
+    if(this.state.selected.includes(this.state.selector)){
+      this.setState({
+        selector: parseInt(Math.random()*15),
+      })
+    }
+    this.nextquestion()
+  }
+
+  nextquestion = () =>{
+    for (let i = 0; i < 4; i++) {
+      this.state.isCorrect[i] =  '#1d1e22'
+    }
+    if(this.state.count<=4 ){
+      this.setState({
+        question: Questions[this.state.selector].question,
+        options: Questions[this.state.selector].options,
+        answer: Questions[this.state.selector].answer,
+        selected: [...this.state.selected, this.state.selector],
+        selector: parseInt(Math.random()*15),
+        press: false,
+        count: this.state.count+1,
+      })
+    }
+    else{
+      this.setState({
+        screen: 'finish',
+      })
+    }
+  }
+
+  finish = () =>{
+    this.setState({
+      screen: 'start',
+      press: false,
+      options: [],
+      count: 1,
+      selector: parseInt(Math.random()*15),
+      selected: [],
+      score: 0
+    })
+  }
+
+  checkAnswer = (option) =>{
+    if(option==this.state.answer){
+      this.state.isCorrect[option]= 'green';
+      this.setState({score: this.state.score+1});
+    }
+    else{
+      this.state.isCorrect[option]= 'red';
+      this.state.isCorrect[this.state.answer]= 'green';
+    }
+  }
   render(){
-  return (
-    <NavigationContainer>
-    <Drawer.Navigator >
-      <Drawer.Screen name="Home" 
-      component={Extreme} 
-      options={{ 
-        drawerLabel: 'Main Menu' 
-      }}
-      />
-      <Drawer.Screen 
-      name="Worldwide"
+    if(this.state.screen==='start'){
+      return(
+          <View style={styles.starts}>
+            <Text style={styles.headingText}> Welcome to Quiz App</Text>
+            <TouchableOpacity 
+            style={styles.startbutton}
+            onPress={()=> this.startquiz()}>
+              <Text style={styles.buttontext}>Start Quiz</Text>
+            </TouchableOpacity>
+          </View>
+      )
+    }
+    else if(this.state.screen==='finish'){
+      return(
+          <View style={styles.starts}>
+            <Text style={styles.finalScore}>Your score is {this.state.score}/5</Text>
+            <Text> </Text>
+            <TouchableOpacity 
+            style={styles.startbutton}
+            onPress={()=> this.finish()}>
+              <Text style={styles.buttontext}> Main Menu </Text>
+            </TouchableOpacity>
+          </View>
+      )
+    }
+    else{
+      let alias = this
+      const allOptions = Object.keys(alias.state.options).map(function(obj){
+        return(
+        <TouchableOpacity 
+        style={
+          {backgroundColor: alias.state.isCorrect[obj],
+           borderRadius: 50, 
+           margin: 2,
+           width: 300,
+           height: 60
+          }}
+        onPress={()=> {alias.setState({press: true}), alias.checkAnswer(obj)}}
+        disabled={alias.state.press===true}>
+        <Text style={styles.opt}>{alias.state.options[obj]}</Text>
+        </TouchableOpacity>
+        )
+      })
+    return(
+      <View >
+        <View style={styles.header}>
+          <Text style={styles.head}>Score: {this.state.score}</Text>
+          <Text style={styles.head}>Question {this.state.count}/5</Text>
+        </View>
+          <View style={styles.container}>
+            <Text style={styles.questiontext}>{this.state.question}</Text>
+            {allOptions}
+          </View>
+          <View style={styles.end}>
+            <TouchableOpacity 
+              style={styles.next} 
+              disabled={this.state.press===false}
+              onPress={()=> this.proceed()}>
+              <Text style={styles.nextText}> --> </Text>
+            </TouchableOpacity>
+          </View>
+      </View>
+    )}
+  }
+}
+
+const styles = StyleSheet.create({
+  finalScore: {
+    fontSize: 30,
+    paddingBottom: 20,
+    color: '#d4d4dc'
+  },
+  background: {
+    flex: 1,
+  },
+  headingText:{
+    fontSize: 30,
+    paddingBottom: 20,
+    color: '#d4d4dc'
+  },
+  starts: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: "#1d1e22"
+  },
+  startbutton: {
+    backgroundColor: '#feda6a',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 200,
+    height: 60,
+    borderRadius: 50,
+    marginTop: 30
+  },
+  buttontext: {
+    fontSize: 24,
+    color: '#1d1e22'
+  },
+  header: {
+    padding: 30,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: "#1d1e22"
+  },
+  head:{
+    fontSize: 14,
+    fontWeight: 'bold',
+    color: '#d4d4dc'
+  },
+  container: {
+    display:'flex',
+    marginTop: 10,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  questiontext: {
+    padding: 20,
+    marginBottom: 50,
+    color: 'black',
+    fontSize: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
     
-      component={Worldwide} 
-      options={{drawerLabel: 'Global Epedemic Condition',title: "Global Summary" }}
-       />
-      <Drawer.Screen 
-      name="continent" 
-      component={continent} 
-      options={{ drawerLabel: 'Condition across Continents' }}
-      />
-    </Drawer.Navigator>
-    </NavigationContainer>
-  );
-}
+  },
+ 
+  opt: {
+    marginLeft: 10,
+    fontSize: 18,
+    fontWeight: "bold",
+    color: 'white',
+    padding: 20,
+  },
+  end: {
+    marginTop: 20,
+    justifyContent: 'flex-end',
+    alignItems:'flex-end'
+  },
+  next: {
+    marginRight: 10,
+    backgroundColor: '#feda6a',
+    justifyContent: 'center',
+    alignItems: "center",
+    width: 100,
+    height: 40,
+    borderRadius: 20,
+  },
+  nextText: {
+    fontSize: 20,
+    color: '#1d1e22'
+  }
+});
 
-
-}
+export default App;
